a99746f3a9d625966cae65970d6f6b592ac1377b
mahin-ahmed <developer1mahin@gmail.com>
2023-07-22 21:19:42 +0600

    Richee final version

---
+++ assets/js/main.js
@@ -0,0 +1,178 @@
+jQuery(document).ready(function($){
+
+
+if ( typeof ProgressBar == 'function' ) { 
+      
+      //PROGRESS-BAR PLUGIN RUN
+     $(".progress-bar").ProgressBar(); 
+}
+  
+  
+if ( typeof circlechart == 'function') {
+
+  //PROGRESS CIRCLE WITH SCROLLING EFFECTS
+
+ $(window).on('scroll',function(){
+        if($(window).scrollTop()>=600){
+         $('.circlechart').circlechart();
+
+   }
+   });
+
+}
+
+
+if ( typeof onePgaeNav == 'function') {
+  
+  //ONE PAGE PLUGIN RUN
+ $('.header-area .menu-item-type-custom .nav-link').onePgaeNav({
+  wrapper: '.navbar-nav',
+  speed: 400,
+ });
+}
+
+
+if ( typeof owlCarousel == 'function') {
+
+  //OWL CAROUSEL FOR TESTIMONIALS
+
+ $(".owl-carousel").owlCarousel({
+
+    autoplay:true,
+    autoplayTimeout:4000,
+    nav:true,
+    items:3,
+     responsiveClass:true,
+          responsive:{
+              // breakpoint from 0 up
+              0:{
+                  items:1,
+                  nav:false,
+                  dots:true
+              },
+              580:{
+                items:1,
+                nav:true,
+                dots:false,
+              },
+              // breakpoint from 768 up
+              768:{
+                  items:2,
+                  nav:true,
+                  dots:false,
+              },
+              // breakpoint from 1000 up
+              1000:{
+                  items:3,
+                  nav:true,
+                  dots:false,
+              }
+          }
+ });
+
+}
+
+
+if ( typeof autofilter == 'function' ) {
+
+  //Auto filter plugin initialize
+
+ $.autofilter({
+
+  // duration in ms
+  duration: 400
+  
+});
+ 
+}
+
+
+//STICKY NAV WITH SCROLLING EFFECTS
+ $(window).on('scroll',function(){
+
+  if($(window).scrollTop()>=150 && !$('.header-area').hasClass('sticky')){
+   $('.header-area').addClass('sticky');
+   $('.sticky').slideDown();
+  }
+
+
+  else if($(window).scrollTop()<=80){
+    
+   $(".header-area").css("display","");
+   if($('.header-area').hasClass('sticky')){
+    $('.header-area').removeClass("sticky");
+   }
+  }
+
+  else if($(window).scrollTop()<150 && $('.header-area').hasClass('sticky')){
+  
+   $('.header-area').removeClass("sticky");
+  }
+
+ });
+
+//TOGGLE MENU SETTINGS
+
+ $('.menu-btn-icon').click(function(){
+  $('.nav-bar-area').toggle(400);
+
+ })
+
+$( window ).resize(function() {
+     var wid=$(window).width();
+     if(wid>992){
+      $('.nav-bar-area').removeAttr('style');
+     }
+});
+ 
+
+
+//menu item active class
+ $('#menu-primary-menu li .nav-link').click(function(){
+    $('li .nav-link').removeClass("active");
+    $(this).addClass("active");
+});
+
+
+// Submenu show on hover
+
+$('.nav-item-has-children').hover(function(){
+  $(this).find('.sub-menu').first().show();
+},function(){
+  $(this).find('.sub-menu').first().hide();
+  
+});
+
+// Submenu show on Focus
+
+
+$('.nav-item-has-children a').focusin(function(){
+
+  $(this).parent().find('.sub-menu').first().css('display','block');
+
+  $(this).parent().find('.nav-item .nav-link').last().focusout(function(){
+    $(this).parent().parent().css('display','none');
+   // $(this).parent().parent().find('.sub-menu').css('display','none');
+  });
+});
+
+
+
+
+
+
+$('.nav-item-has-children').find('.nav-item-has-children').find('.sub-menu').css({'left':'-100%','top':'5px'});
+
+
+
+ //portfolio button active class
+
+ $('.portfolio-button #portfolio-category').click(function(){
+  
+    $('.portfolio-button #portfolio-category').removeClass("active");
+
+    $(this).addClass("active");
+    
+});
+
+});
---
+++ functions.php
@@ -0,0 +1,216 @@
+<?php
+/**
+ * richee functions and definitions
+ *
+ * @package richee
+ */
+
+/*
+*TGM LIBRARY 
+*/
+require_once get_theme_file_path().'/inc/richee-tgm.php';
+/*
+*Kirki Customizer settings
+*/
+require_once get_theme_file_path().'/inc/richee-customizer.php';
+/*
+*Richee Demo Import 
+*/
+require_once get_theme_file_path().'/inc/richee-demo-import.php';
+
+if ( ! defined( '_RICHEE_VERSION' ) ) {
+	// Replace the version number of the theme on each release.
+	define( '_RICHEE_VERSION', '1.0.0');//'1.0.0');
+}
+
+
+function richee_setup() {
+	/*
+		* Make theme available for translation.
+		* Translations can be filed in the /languages/ directory.
+		* If you're building a theme based on richee, use a find and replace
+		* to change 'richee' to the name of your theme in all the template files.
+		*/
+	load_theme_textdomain( 'richee', get_template_directory() . '/languages' );
+
+	add_theme_support( 'automatic-feed-links' );
+
+	add_theme_support( 'title-tag' );
+
+	add_theme_support( 'align-wide' );
+
+	add_theme_support( "wp-block-styles" );
+
+	add_theme_support( 'post-thumbnails' );	
+		
+	add_theme_support( 'editor-styles' );
+
+	add_editor_style( 'style-editor.css' );
+
+	add_theme_support( "responsive-embeds" );
+
+	 add_theme_support( "custom-header");
+
+	 add_theme_support( "custom-background");
+
+	// This theme uses wp_nav_menu() in one location.
+	register_nav_menus(
+		array(
+			'richee_primary' => esc_html__( 'Primary Menu', 'richee' ),
+		)
+	);
+
+	/*
+		* Switch default core markup for search form, comment form, and comments
+		* to output valid HTML5.
+		*/
+	add_theme_support(
+		'html5',
+		array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+			'style',
+			'script',
+		)
+	);
+
+	add_theme_support( 'html5', array( 'navigation-widgets' ) );
+
+	
+
+	// Add theme support for selective refresh for widgets.
+	add_theme_support( 'customize-selective-refresh-widgets' );
+
+	/**
+	 * Add support for core custom logo.
+	 *
+	 */
+	add_theme_support(
+		'custom-logo',
+		array(
+			'height'      => 250,
+			'width'       => 250,
+			'flex-width'  => true,
+			'flex-height' => true,
+		)
+	);
+}
+add_action( 'after_setup_theme', 'richee_setup' );
+
+
+
+
+/**
+ * Enqueue scripts and styles.
+ */
+function richee_enqueue_scripts() {
+
+	wp_enqueue_style( 'richee-bootstrap', get_template_directory_uri().'/assets/css/bootstrap.min.css', array());
+	
+	wp_enqueue_style( 'richee-fontawesome', get_template_directory_uri().'/assets/css/all.min.css', array());
+
+	if('front-page-template.php' == basename(get_page_template())){
+
+		wp_enqueue_style( 'richee-progress-bar', get_template_directory_uri().'/assets/css/progress-bar.css', array());
+
+		wp_enqueue_style( 'richee-owl-carousel', get_template_directory_uri().'/assets/css/owl.carousel.min.css', array());
+
+		wp_enqueue_style( 'richee-owl-carousel-theme', get_template_directory_uri().'/assets/css/owl.theme.default.min.css', array());
+
+	}
+
+	wp_enqueue_style( 'richee-template-style', get_template_directory_uri().'/assets/css/style.css', array(),_RICHEE_VERSION);
+
+	wp_enqueue_style( 'richee-google-fonts','//fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap', array());
+
+	wp_enqueue_script( 'comment-reply' );
+
+	wp_enqueue_style( 'richee-style',get_stylesheet_uri(), array(),_RICHEE_VERSION,false);
+	
+	//JAVASCRIPT FILE ENQUEUE
+
+	if('front-page-template.php' == basename(get_page_template())){
+
+		wp_enqueue_script( 'richee-bootstrap', get_template_directory_uri().'/assets/js/bootstrap.min.js', array('jquery'), _RICHEE_VERSION, true );
+
+		wp_enqueue_script( 'richee-animate-typing', get_template_directory_uri().'/assets/js/jquery.animateTyping.js', array('jquery'), _RICHEE_VERSION, true );
+
+		wp_enqueue_script( 'richee-progressbar', get_template_directory_uri().'/assets/js/progress-bar.js', array('jquery'), _RICHEE_VERSION, true );
+
+		wp_enqueue_script( 'richee-progressbar-circle', get_template_directory_uri().'/assets/js/progresscircle.js', array('jquery'), _RICHEE_VERSION, true );
+
+		wp_enqueue_script( 'richee-onepage-nav', get_template_directory_uri().'/assets/js/onepageNav.js', array('jquery'), _RICHEE_VERSION, true );
+
+		wp_enqueue_script( 'richee-owl-carousel', get_template_directory_uri().'/assets/js/owl.carousel.min.js', array('jquery'), _RICHEE_VERSION, true );
+
+		wp_enqueue_script( 'richee-autofilter-js', get_template_directory_uri().'/assets/js/autofilter.js', array('jquery'), _RICHEE_VERSION, true );
+
+	}
+	wp_enqueue_script( 'richee-main-js', get_template_directory_uri().'/assets/js/main.js', array('jquery'), _RICHEE_VERSION, true );
+
+}
+add_action( 'wp_enqueue_scripts', 'richee_enqueue_scripts' );
+
+function richee_theme_slug_widgets_init() {
+	register_sidebar( array(
+		'name'          => __( 'Blog Sidebar', 'richee' ),
+		'id'            => 'richee_blog_sidebar',
+		'description'   => __( 'Footer Widget', 'richee' ),
+		'before_widget' => '',
+		'after_widget'  => '',
+		'before_title'  => '<h2 class="widgettitle">',
+		'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'richee_theme_slug_widgets_init' );
+
+
+
+function richee_head_css(){
+	?>
+
+	<style>
+		.hero-intro{
+			background-image: url(<?php echo esc_url(get_template_directory_uri().'/assets/images/hero-bg.png');?>);
+		}
+	</style>
+
+	<?php
+}
+add_action('wp_head','richee_head_css');
+
+
+function richee_the_posts_navigation() {
+		the_posts_pagination(
+			array(
+				'mid_size'           => 5,
+				'prev_text'          => sprintf(
+					'%s <span class="nav-prev-text">%s</span>','<i class="fa-solid fa-arrow-left"></i>',
+					wp_kses(
+						__( 'Newer <span class="nav-short">posts</span>', 'richee' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					)
+				),
+				'next_text'          => sprintf(
+					'<span class="nav-next-text">%s</span> %s',
+					wp_kses(
+						__( 'Older <span class="nav-short">posts</span>', 'richee' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					'<i class="fa-solid fa-arrow-right"></i>'
+				),
+			)
+		);
+	}
+
